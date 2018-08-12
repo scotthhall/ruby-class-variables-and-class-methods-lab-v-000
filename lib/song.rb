dfa72781fd4623e22fb61ace464359d3c6d0d96e
Scott Hall <scotthall127@gmail.com>
2018-08-12 15:27:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,17 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genres = []
 
   def initialize(name, artist, genre)
     @@count += 1
+    @@genres << genre unless @@genres.include? 'self'
   end
 
   def count
     @@count
   end
 
+  def genres
 
 end
